af2d94bf4523edb404484f8025ea8be63b11dbf5
Alexans-Soul <alexgizow@gmail.com>
2026-08-26 20:19:00 +0300

    Selected Object Indicator

---
+++ Services/SelectedObjectVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BobMapper.Services
+{
+    public class SelectedObjectVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return Visibility.Collapsed;
+
+            return ReferenceEquals(values[0], values[1])
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
